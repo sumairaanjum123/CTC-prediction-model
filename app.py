83ec98f95288b91e9fff9ed4e9d2554d46a4b9c7
sumairaanjum123 <sumairaanjum550@gmail.com>
2025-12-05 14:57:00 +0530

    Rename app title to 'CTC Predictor App'

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 # Load the model
 model = joblib.load("model.pkl")
 
-st.title("Simple Linear Regression - Real Time Predictor")
+st.title("CTC Predictor App")
 
 # Input
 x = st.number_input("Enter Years of Experience :", value=0.0)
@@ -14,3 +14,4 @@ x = st.number_input("Enter Years of Experience :", value=0.0)
 if st.button("Predict"):
     prediction = model.predict([[x]])[0]
     st.success(f"Predicted CTC = {prediction:.2f}")
+
